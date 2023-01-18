a00c90dd4fb3dd0e33024543b8e86c6c4c9112aa
Emmanuel-Cor-Eus01 <119893044+Emmanuel-Cor-Eus01@users.noreply.github.com>
2023-01-17 21:33:35 -0600

    add martes-clase
    ejercicio de la clase

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,21 @@
-console.log("hola mundo js")
+"use strict";
+console.log("hola mundo js");
+var nombre = "Juan";
+var edad = 30;
+
+const fechaNacimiento = "01/06/1995";
+
+let appellido = "Perez";
+console.log(fechaNacimiento)
+
+let nombreCompleto = "Jesus Cardenas"; // string(texto)
+let telefono = 123456789; // number(numero)
+let humano = true; // boolean(true/false)
+let direccion; //undefined
+let codigoPostal = null; // null
+
+console.log("nombreCompleto",  nombreCompleto);
+console.log("telefono",  telefono);
+console.log("humano",  humano);
+console.log("direccion",  direccion);
+console.log("codigoPostal",  codigoPostal);
---
+++ assets/js/main2.js
@@ -0,0 +1,14 @@
+let carName = "Volvo";
+let x = 50;
+let a= 5;
+let b = 10;
+let s = a + b;
+let x1 = "x";
+let y = "y";
+let z = x1 + y;
+
+console.log("cargado desde el main2");
+console.log(carName);
+console.log(x);
+console.log(s);
+console.log(z);
